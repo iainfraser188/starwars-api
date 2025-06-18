025d4de9e7b7750924b0ad51031ecdc07d50e087
OPTI-IFraser <ifraser@optiseller.com>
2025-06-18 12:16:39 +0100

    added films and people pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,8 @@ function Navbar() {
             <h1>Star Wars Api</h1>
             <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
                 <Link to="/">Home</Link>
+                <Link to="/Films">Films</Link>
+                <Link to="/People">People</Link>
             </nav>
         </>       
     );
---
+++ src/pages/Films.jsx
@@ -0,0 +1,10 @@
+function Films() {
+    return (
+        <div>
+            <h1>Films</h1>
+        </div>
+
+    );
+}
+
+export default Films;
